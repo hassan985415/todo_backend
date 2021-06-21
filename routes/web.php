6e3c7d305a3065c7280c73868b4583fb0a408c05
hassan985415 <83587444+hassan985415@users.noreply.github.com>
2021-06-21 15:46:23 +0500

    todobackend

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "TODO API";
 });
-
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
-Route::resource('todo', \App\Http\Controllers\TodoController::class);
